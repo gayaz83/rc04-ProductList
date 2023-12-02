d5d067bbec675bef55d628d9c1f5bb8aa40e6d2d
gayaz83 <gokhanayaz1983@gmail.com>
2023-12-02 12:03:09 +0100

    scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/header/Header"
 import ProductList from "./components/products/ProductList"
+import {products,categories} from "./helper/data"
+import "./App.scss"
+
 
 function App() {
+  console.log(categories);
+  console.log(products);
   return (
     <div>
-      <Header />
-      <ProductList />
+      <Header categories={categories} text="Products List" />
+      <ProductList products={products}/>
     </div>
   )
 }
